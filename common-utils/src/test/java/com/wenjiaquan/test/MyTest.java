f6ba32b9b16fdb1f1c3b35ca3e790fa4a7a1d737
wenjiaquan <1162826010@qq.com>
2019-12-07 10:25:50 +0800

    测试类

---
+++ common-utils/src/test/java/com/wenjiaquan/test/MyTest.java
@@ -0,0 +1,20 @@
+package com.wenjiaquan.test;
+
+import org.junit.Test;
+
+import com.wenjiaquan.utils.StringUtil;
+
+/**   
+* @Title: MyTest.java 
+* @Package com.wenjiaquan.test 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author A18ccms A18ccms_gmail_com   
+* @date 2019年12月7日 上午8:48:34 
+* @version V1.0   
+*/
+public class MyTest {
+	@Test
+	public void DateUtil() {
+		System.out.println(StringUtil.isBlank("ssss"));
+	}
+}
