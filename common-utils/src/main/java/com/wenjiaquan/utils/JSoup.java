f7523fd043c225fc014ba8796469cd8371d97135
dell <dell@192.168.124.1>
2020-02-14 10:02:48 +0800

    添加搜狐爬虫

--- common-utils/src/main/java/com/wenjiaquan/utils/JSoup.java
+++ common-utils/src/main/java/com/wenjiaquan/utils/JSoup.java
@@ -143,4 +143,41 @@ public class JSoup {
 		}
 		System.out.println("首页中找到了复合条件的网址有：" + count + "篇文章");
 	}
+	public static void sohu() throws IOException {
+		String file_path="D:\\1709DJsoup";
+		//模拟浏览器发请求
+				Connection connect = Jsoup.connect("http://news.sohu.com/");
+				Document doc = connect.get();
+//				System.out.println(doc);
+				Elements select = doc.select(".list16");
+				for (Element list16 : select) {
+//					System.out.println(list16);
+					Elements select2 = list16.select("a[href]");
+					for (Element a : select2) {
+//						System.out.println(a);
+						String url = a.attr("href");
+						System.out.println(url);
+						if(!url.startsWith("http")) {
+							url="http:"+url;
+						}
+//						System.out.println(url);
+						if(!url.contains("subject")&&!url.contains("sports")) {
+							Connection connect2 = Jsoup.connect(url);
+							Document document = connect2.get();
+							Elements select3 = document.select(".article");
+							String content=null;
+							for (Element element : select3) {
+								//System.out.println(element);
+								content = element.text();
+//								System.out.println(content);
+							}
+							String title = a.attr("title");
+							 title = title.replace("|", "").replace("*", "").replace("\"", "").replace("?", "").replace("/", "")
+							.replace("\\", "").replace(">", "").replace("<", "").replace(":", "");
+//							 以标题作为文本的名称,已内容作为文本的内容存在本地磁盘
+							 FileUtil.writeFile(file_path + "\\" + title + ".txt", content, "utf8");
+						}
+					}
+				}
+	}
 }
